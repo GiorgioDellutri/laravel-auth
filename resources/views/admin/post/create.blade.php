b1e4445fc60502636aa558745d1fe3589b1df0c6
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-21 19:20:58 +0100

    fix include

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div class="container my-5">
-        @include('admin.partials.editCreate', ['methods' => 'POST', 'routeName' => 'admin.post.store'])
+        @include('admin.partials.editCreate', ['methods' => '', 'routeName' => 'admin.post.store'])
     </div>
 @endsection
