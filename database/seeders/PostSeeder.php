a2e6ce5b1f5fc7db687b72952ef545ec53cc2960
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-21 02:45:58 +0100

    created PostSeeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str as Str;
 
 class PostSeeder extends Seeder
 {
@@ -12,8 +15,17 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 60; $i++) {
+            $newPost = new Post();
+            $newPost->slug = Str::slug($newPost->title);
+            "Ciao a tutti ";
+            $newPost->title = $faker->unique()->realTextBetween(5, 20);
+            $newPost->author = $faker->name();
+            $newPost->content = $faker->realTextBetween(600, 800);
+            $newPost->post_date = $faker->dateTimeThisYear();
+            $newPost->save();
+        }
     }
 }
