b0de83d9eecac71955e0141742d58ccde6fad69a
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-22 16:08:45 +0100

    added compact

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $posts = Post::all();
+        return view('admin.dashboard', compact('posts'));
     }
 }
